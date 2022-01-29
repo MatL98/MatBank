6a0b852e35c2db42e1c280851dc7116fb962c3c1
MatL98 <longomaty@gmail.com>
2022-01-29 20:41:00 -0300

    add env

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,8 +1,9 @@
 const { Sequelize } = require('sequelize');
+require("dotenv").config()
 
-const sequelize = new Sequelize("bank", "root", "", {
-  dialect:"mysql",
-  host: "localhost",
+const sequelize = new Sequelize(process.env.DB_DB, process.env.DB_USER,process.env.DB_PASSWORD, {
+  dialect:process.env.DB_DIALECT,
+  host:process.env.DB_HOST,
 })
 
 
