89c7b6cdc3c334ab2d87ec6e216f6c822f9e5284
MatL98 <longomaty@gmail.com>
2022-03-27 22:39:47 -0300

    add backend test

--- server/src/routes/login.js
+++ server/src/routes/login.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const { authLogin, authSignUp, authLogOut } = require("../controllers/loginController");
-const { verifyToken } = require("../middlewares/auth");
 const { Router } = express;
 const router = new Router();
 
